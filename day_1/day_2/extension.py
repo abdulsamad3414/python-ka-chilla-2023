bac439b1c7f089b589f5a6f074a95866e8a3f85a
abdulsamad3414 <your_no-reply_email@users.noreply.github.com>
2024-12-20 15:13:00 +0500

    modification

--- day_1/day_2/extension.py
+++ day_1/day_2/extension.py
@@ -1,11 +1,12 @@
 # Top 10 Extension 
 
 # 1-Python 
-# 2-intellicode
+# 2-intellicode   used for automatic code 
 # 3-jupyter
 # 4-jupyter keymap
-# 5-codesnap
+# 5-codesnap   used for bharam bazi in code 
 # 6-markdown all in one yuzhang wali
 # 7-prettier code formatter
-# 8-blackbox
-
+# 8-blackbox   AI baesed automaic tool ha .....# //how to calculate square in python give me code ?
+# 9- codeium
+# //how to calculate square in python give me code 
